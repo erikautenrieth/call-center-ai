31612722108482a034ee28c5f33d28cce584abfb
Erik Autenrieth <53609156+erikautenrieth@users.noreply.github.com>
2025-09-07 12:56:50 +0200

    change language to DE and EN only and change lexicon.xml to DE

--- app/helpers/config_models/conversation.py
+++ app/helpers/config_models/conversation.py
@@ -43,22 +43,12 @@ class LanguageModel(BaseModel):
         LanguageEntryModel(
             pronunciations_en=["German", "DE", "Germany"],
             short_code="de-DE",
-            voice="de-DE-ConradNeural",
+            voice="de-DE-FlorianMultilingualNeural",
         ),
         LanguageEntryModel(
             pronunciations_en=["English", "EN", "United States"],
             short_code="en-US",
-            voice="en-US-ShimmerTurboMultilingualNeural",
-        ),
-        LanguageEntryModel(
-            pronunciations_en=["Spanish", "ES", "Spain"],
-            short_code="es-ES",
-            voice="es-ES-ArabellaMultilingualNeural",
-        ),
-        LanguageEntryModel(
-            pronunciations_en=["Chinese", "ZH", "China"],
-            short_code="zh-CN",
-            voice="zh-CN-XiaoyuMultilingualNeural",
+            voice="en-US-AlloyTurboMultilingualNeural",
         ),
     ]
 
