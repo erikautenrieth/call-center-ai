fb73efd53965053bd5f9dd7ba7d42a200521f750
Thomas Gerlach <thomas.gerlach@uni-koeln.de>
2025-09-26 13:54:08 +0000

    change(to old audio and bidirectional)

--- app/main.py
+++ app/main.py
@@ -412,7 +412,8 @@ async def call_post(request: Request) -> CallGetModel:
         start_media_streaming=False,
         transport_type=StreamingTransportType.WEBSOCKET,
         transport_url=wss_url,
-        audio_format=AudioFormat.PCM24_K_MONO,
+        enable_bidirectional=True,
+        audio_format=AudioFormat.PCM16_K_MONO,
     )
 
     logger.info("SDK initialized ")
